44d2d3b4e066d431134e14280b1d4cabbce24962
Jordan Buranskas <jordanburanskas@gmail.com>
2023-02-24 14:42:30 -0500

    app controller finished

--- Reddit/app/controllers/application_controller.rb
+++ Reddit/app/controllers/application_controller.rb
@@ -12,19 +12,24 @@ class ApplicationController < ActionController::Base
    
     def log_out!
         current_user.reset_session_token if logged_in?
-    
-    
+        session[:session_token] = nil 
+        @current_user = nil 
+        #how do we have access to @current_user
     end
     
     def logged_in?
-    
+        !!current_user
     end
     
     def require_logged_out
-    
+
+        redirect_to users_url if logged_in?
+
     end
     
     def require_logged_in
+
+        redirect_to new_session_url unless logged_in?
     
     end
 end
